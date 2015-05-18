dcdec3988a1dc494862a4357c4e8138d6b7d2405
James Fowkes <jamesfowkes@gmail.com>
2015-05-18 16:40:10 +0100

    Added database migrations

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -21,4 +23,3 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    
